6b12ed5e7ca2933a681fc98609c82a6ef121d46c
fjur <fjuristovski@gmail.com>
2016-03-21 19:33:48 +0000

    Songs up and showing as well as song detail panel

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,29 @@
 get '/' do
   erb :index
 end
+
+get '/music' do
+  @songs = Song.all
+  erb :'music/index'
+end
+
+get '/music/new' do
+  erb :'music/new'
+end
+
+get '/music/:id' do
+  @song = Song.find params[:id]
+  erb :'music/show'
+
+end
+
+post '/music' do
+    @songs = Song.new(
+      song_title: params[:song_title],
+      author: params[:author],
+      url: params[:url]
+    )
+  @songs.save
+  redirect '/music'
+end
+

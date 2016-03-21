5f4516e2f0a5437cef718537780e46e3850c5ef0
fjur <fjuristovski@gmail.com>
2016-03-21 20:24:52 +0000

    added users and login/logout

--- app/actions.rb
+++ app/actions.rb
@@ -1,3 +1,11 @@
+helpers do
+  def current_user
+    if session[:user_id]
+      User.find(session[:user_id]).email
+    end
+  end
+end
+
 # Homepage (Root path)
 get '/' do
   erb :index
@@ -15,7 +23,6 @@ end
 get '/music/:id' do
   @song = Song.find params[:id]
   erb :'music/show'
-
 end
 
 post '/music' do
@@ -28,3 +35,23 @@ post '/music' do
   redirect '/music'
 end
 
+post '/signup' do
+  user = User.create(email: params[:email], password: params[:password])
+  session[:user_id] = user.id
+  redirect '/music'
+end
+
+post '/logout' do
+  session[:user_id] = nil
+  redirect '/music'
+end
+
+post '/login' do
+  user = User.find_by(email: params[:email])
+  if user && user.password = params[:password]
+    session[:user_id] = user.id
+    redirect '/music'
+  else
+
+  end
+end

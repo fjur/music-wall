26ab1f4d6660a60735104196c540ddf550bfd02f
fjur <fjuristovski@gmail.com>
2016-03-21 20:27:02 -0700

    using bootstrap

--- app/actions.rb
+++ app/actions.rb
@@ -12,7 +12,7 @@ get '/' do
   erb :index
 end
 
-get '/music' do
+get '/songs' do
   @songs = Song.all
   erb :'music/index'
 end
@@ -21,7 +21,7 @@ get '/music/new' do
   erb :'music/new'
 end
 
-get '/music/:id' do
+get '/music' do
   @song = Song.find params[:id]
   erb :'music/show'
 end
@@ -36,7 +36,7 @@ post '/music' do
       @songs.user_id = session[:user_id]
     end
   @songs.save
-  redirect '/music'
+  redirect '/songs'
 end
 
 post '/upvote/:id' do
@@ -45,25 +45,30 @@ post '/upvote/:id' do
   vote.update(song_id: @song.id, user_id: current_user.id)
   # vote.song_id = @song.id
   # vote.save
-  redirect '/music'
+  redirect '/songs'
 end
 
 post '/signup' do
   user = User.create(email: params[:email], password: params[:password])
   session[:user_id] = user.id
-  redirect '/music'
+  redirect '/songs'
 end
 
 post '/logout' do
   session[:user_id] = nil
-  redirect '/music'
+  redirect '/songs'
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/songs'
 end
 
 post '/login' do
   user = User.find_by(email: params[:email])
   if user && user.password = params[:password]
     session[:user_id] = user.id
-    redirect '/music'
+    redirect '/songs'
   else
 
     #  <% if song.song_title == 'fusion' %>

537492910f5be235e07a9e42e7b2433f68d7d96c
fjur <fjuristovski@gmail.com>
2016-03-22 19:50:02 +0000

    Done stars, thanks maher

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,6 @@ class Review < ActiveRecord::Base
   belongs_to :user
   belongs_to :song
 
+  validates :rating, inclusion: {in: 1..6}
+
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
 
+   $("#rateYo").rateYo({
+ 
+    onChange: function (rating, rateYoInstance) { 
+      // $(this).next().text(rating);
+      // var numStars = $("#rateYo").rateYo("option", "numStars"); //returns 10
+      $("#review-rating").val(rating + 1);
+    }
+  });
+
+   
+
+
+
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });

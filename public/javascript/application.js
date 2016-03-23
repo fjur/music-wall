06633d1ea25fee1260221a85243f1ed16544009e
fjur <fjuristovski@gmail.com>
2016-03-23 06:07:33 +0000

    Cleaned up css and added dynamic css

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
-   $("#rateYo").rateYo({
+   $(".rateYo").rateYo({
  
     onChange: function (rating, rateYoInstance) { 
       // $(this).next().text(rating);
       // var numStars = $("#rateYo").rateYo("option", "numStars"); //returns 10
-      $("#review-rating").val(rating + 1);
+      $(".review-rating").val(rating + 1);
     }
   });
 
